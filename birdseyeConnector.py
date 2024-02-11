1188cccffb2f230175ff9013ca60f417280f897e
TheKneesBees <aarontfleischer@yahoo.com>
2024-02-11 02:38:23 -0800

    Pulled in new class for snes controller into birdseyeConnector

--- birdseyeConnector.py
+++ birdseyeConnector.py
@@ -1,45 +1,11 @@
 import birdseyelib as bird
 import time
-from Game import gameMemoryState
+from Game import gameMemoryState, SNESControllerInput
 
 HOST = ""
 
 HOST = "127.0.0.1"
 PORT = 8080
-
-class SNESControllerInput(bird.ControllerInput):
-    def __init__(self, client) -> None:
-        self.client = client
-
-    def set_controller_input(self, a=False, b=False, x=False, y=False, up=False, down=False, right=False, left=False, start=False, select=False):
-        """Sets the controller inputs to be executed in the emulator.
-        All inputs are set to `False` be default.
-        The inputs are executed until a new controller input is sent.
-
-        :param a: The state of the A button.
-        :type a: bool
-
-        :param b: The state of the B button.
-        :type b: bool
-
-        :param up: The state of the Up button on the control pad.
-        :type up: bool
-
-        :param down: The state of the Down button on the control pad.
-        :type down: bool
-
-        :param right: The state of the Right button on the control pad.
-        :type right: bool
-
-        :param left: The state of the Left button on the control pad.
-        :type left: bool"""
-        bool_to_string = {False : "false", True : "true"}
-        controller_input = bool_to_string[a] + ";" + bool_to_string[b] + ";" + \
-                           bool_to_string[x] + ";" + bool_to_string[y] + ";" + \
-                           bool_to_string[up] + ";" + bool_to_string[down] + ";" + \
-                           bool_to_string[right] + ";" + bool_to_string[left] + ";" +\
-                           bool_to_string[start] + ";" + bool_to_string[select]
-        self.client._queue_request("INPUT;" + controller_input + "\n")
 
 def sub_game_current(mem1, mem2):
     match mem1, mem2:
@@ -62,7 +28,7 @@ if __name__ == "__main__":
     gameState = gameMemoryState.gameMemoryState()
 
     memory = bird.Memory(client)
-    controller_input = SNESControllerInput(client)
+    controller_input = SNESControllerInput.SNESControllerInput(client)
     emulation = bird.Emulation(client)
     external_tool = bird.ExternalTool(client)
 
